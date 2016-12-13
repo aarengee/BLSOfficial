7251526a64351895312ca7263288d46f08a62ecf
Rijul Gupta <Rijul Gupta>
2016-12-13 15:38:19 +0530

    mod navbar

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ $(document).ready(function(){
        
     var $navbar = $("#xyz"),
         y_pos = $navbar.offset().top,
-        height = $navbar.height();
-
+         height = $navbar.height();
+        winwidth= $(window).width();
+    var $mainopt = $(".mainopt");
+     if(($(window).width())<=768){ $mainopt.removeClass("mmopt");}
+        else if(($(window).width())>768){$mainopt.addClass("mmopt");}
     $(document).scroll(function() {
         var scrollTop = $(this).scrollTop();
 
-        if (scrollTop > y_pos) 
+        if (scrollTop > y_pos + height) 
             $navbar.addClass("fixit");
-        else if (scrollTop <= y_pos) 
+        else if (scrollTop <= (y_pos+height)) 
             $navbar.removeClass("fixit");
                 
     })
+    $( window ).resize(function() {
+        if(($(window).width())<=768){ $mainopt.removeClass("mmopt");}
+        else if(($(window).width())>768){$mainopt.addClass("mmopt");}
+   })
 });
 
 
